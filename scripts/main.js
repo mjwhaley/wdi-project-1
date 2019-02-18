d90bc4e95a175e24dfb1a973f6938cafd6b70315
Matthew Whaley <mjwhaley@mac.com>
2019-02-18 11:04:02 +0000

    Grid added

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
 console.log('Running')
+const gameAreaWidth = 1000
+const gameAreaHeight = 800
+const gameAreaBlockWidth = Math.round(gameAreaWidth/12)
+const gameAreaBlockHeight = Math.round(gameAreaHeight/11)
+console.log(gameAreaBlockWidth)
+console.log(gameAreaBlockHeight)
+
+
+
+function createGrid(x, y) {
+  for (var rows = 0; rows < x; rows++) {
+    for (var columns = 0; columns < y; columns++) {
+      $('.container').append('<div class="grid"></div>')
+    }
+  }
+  $('.grid').width(83)
+  $('.grid').height(73)
+}
+
+$(document).ready(() => {
+  createGrid(12, 11)
+
+})
