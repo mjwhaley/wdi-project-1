f4b07e2e6d7a439a8779cc121c2411410d2ef4af
Matthew Whaley <mjwhaley@mac.com>
2019-02-18 12:30:42 +0000

    Grid completed with H and V

--- scripts/main.js
+++ scripts/main.js
@@ -3,22 +3,31 @@ const gameAreaWidth = 1000
 const gameAreaHeight = 800
 const gameAreaBlockWidth = Math.round(gameAreaWidth/12)
 const gameAreaBlockHeight = Math.round(gameAreaHeight/11)
-console.log(gameAreaBlockWidth)
-console.log(gameAreaBlockHeight)
+const gridVertical = ['v1', 'v2','v3', 'v4','v5', 'v6','v7', 'v8','v9', 'v10', 'v11', 'v12']
+const gridHorizontal = ['h1', 'h2','h3', 'h4','h5', 'h6','h7', 'h8','h9', 'h10', 'h11']
 
+console.log(`Grid Width = ${gameAreaBlockWidth}`)
+console.log(`Grid Height = ${gameAreaBlockHeight}`)
 
-
-function createGrid(x, y) {
-  for (var rows = 0; rows < x; rows++) {
-    for (var columns = 0; columns < y; columns++) {
-      $('.container').append('<div class="grid"></div>')
-    }
-  }
-  $('.grid').width(83)
-  $('.grid').height(73)
+function playGame () {
+  createGrid(11, 12)
 }
 
-$(document).ready(() => {
-  createGrid(12, 11)
+function createGrid(x, y) {
+  for (let rows = 0; rows < x; rows++) {
+    for (let columns = 0; columns < y; columns++) {
+      $('.container').append(`<div class="gameGrid ${gridVertical[columns]} ${gridHorizontal[rows]}"></div>`)
+    }
+  }
+  $('.gameGrid').width(gameAreaBlockWidth)
+  $('.gameGrid').height(gameAreaBlockHeight)
+}
 
+
+
+
+
+
+$(document).ready(() => {
+  playGame()
 })
