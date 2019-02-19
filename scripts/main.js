6dad50111ac12dac2fc9f4fd22dce980e59c6559
Matthew Whaley <mjwhaley@mac.com>
2019-02-19 16:43:32 +0000

    Named Aliens and icons in place.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,15 @@
 //Varibles
-const gameAreaWidth = 1000
-const gameAreaHeight = 800
-const gameAreaBlockWidth = Math.round(gameAreaWidth/12)
-const gameAreaBlockHeight = Math.round(gameAreaHeight/11)
-const gridVertical = ['v1', 'v2','v3', 'v4','v5', 'v6','v7', 'v8','v9', 'v10', 'v11', 'v12']
-const gridHorizontal = ['h1', 'h2','h3', 'h4','h5', 'h6','h7', 'h8','h9', 'h10', 'h11']
+const gameAreaWidth = 984
+const gameAreaHeight = 792
+const gameAreaBlockWidth = Math.round(gameAreaWidth/24)
+const gameAreaBlockHeight = Math.round(gameAreaHeight/22)
+const gridVertical = ['v1','v2','v3','v4','v5','v6','v7','v8','v9','v10','v11','v12','v13','v14','v15','v16','v17','v18','v19','v20','v21','v22','v23','v24']
+const gridHorizontal = ['h1','h2','h3','h4','h5','h6','h7','h8','h9','h10','h11','h12','h13','h14','h15','h16','h17','h18','h19','h20','h21','h22']
 let playerShipLocation = null
 let motherShipLocation = 'v1'
 let motherShipMoverInterval = null
 let motherShipFrequencyInterval = null
+let invadersInterval = null
 const alienShipSpeed = 700
 const motherShipStartDelay = 2000
 const motherShipFrequency = 5000
@@ -25,18 +26,58 @@ function quitGame () {
   $('li.quit').click(function() {
     clearInterval(motherShipMoverInterval)
     clearInterval(motherShipFrequencyInterval)
+    clearInterval()
     //Need to add in the removing of the classes to show the ships.
   })
 }
 function playGame () {
-  createGrid(11, 12)
+  createGrid(22, 24)
+  navRemove()
   playerMove()
   playerFire()
   placePlayerShip()
-  placeAlienShips()
   playerShipLocator()
   setTimeout(launchMotherShip, motherShipStartDelay)
-  aliens = [new Alien(3, 2), new Alien(5, 2), new Alien(7, 2)]
+  aliens = [new Alien(3, 2, 'johnny5_1', 50),
+    new Alien(5, 2, 'johnny5_2', 50),
+    new Alien(7, 2, 'johnny5_3', 50),
+    new Alien(9, 2, 'johnny5_4', 50),
+    new Alien(11, 2, 'johnny5_5', 50),
+    new Alien(13, 2, 'johnny5_6', 50),
+    new Alien(15, 2, 'johnny5_7', 50),
+    new Alien(17, 2, 'johnny5_8', 50),
+    new Alien(3, 4, 'walle_1', 40),
+    new Alien(5, 4, 'walle_2', 40),
+    new Alien(7, 4, 'walle_3', 40),
+    new Alien(9, 4, 'walle_4', 40),
+    new Alien(11, 4, 'walle_5', 40),
+    new Alien(13, 4, 'walle_6', 40),
+    new Alien(15, 4, 'walle_7', 40),
+    new Alien(17, 4, 'walle_8', 40),
+    new Alien(3, 6, 'c3po_1', 30),
+    new Alien(5, 6, 'c3po_2', 30),
+    new Alien(7, 6, 'c3po_3', 30),
+    new Alien(9, 6, 'c3po_4', 30),
+    new Alien(11, 6, 'c3po_5', 30),
+    new Alien(13, 6, 'c3po_6', 30),
+    new Alien(15, 6, 'c3po_7', 30),
+    new Alien(17, 6, 'c3po_8', 30),
+    new Alien(3, 8, 'r2d2_1', 20),
+    new Alien(5, 8, 'r2d2_2', 20),
+    new Alien(7, 8, 'r2d2_3', 20),
+    new Alien(9, 8, 'r2d2_4', 20),
+    new Alien(11, 8, 'r2d2_5', 20),
+    new Alien(13, 8, 'r2d2_6', 20),
+    new Alien(15, 8, 'r2d2_7', 20),
+    new Alien(17, 8, 'r2d2_8', 20),
+    new Alien(3, 10, 'bender_1', 10),
+    new Alien(5, 10, 'bender_2', 10),
+    new Alien(7, 10, 'bender_3', 10),
+    new Alien(9, 10, 'bender_4', 10),
+    new Alien(11, 10, 'bender_5', 10),
+    new Alien(13, 10, 'bender_6', 10),
+    new Alien(15, 10, 'bender_7', 10),
+    new Alien(17, 10, 'bender_8', 10)]
 }
 
 function createGrid(x, y) {
@@ -49,6 +90,10 @@ function createGrid(x, y) {
   $('.gameGrid').height(gameAreaBlockHeight)
 }
 
+function navRemove() {
+  $('H1').slideUp()
+  $('.fade').fadeOut()
+}
 function playerMove () {
   $(document).keydown(function(e) {
     switch(e.which) {
@@ -82,47 +127,88 @@ function placePlayerShip () {
 }
 
 class Alien {
-  constructor(startingV, startingH) {
+  constructor(startingV, startingH, name, score) {
     this.currentV = startingV
     this.currentH = startingH
+    this.name = name
+    this.score = score
+    this.currentMoves = 0
+    this.isMovingRight = true
     this.isHit = false
     this.render()
-    this.movementTimer = null
-    this.movement()
+    this.movementId = null
+    this.move()
   }
 
   render() {
-    console.log($(`.v${this.currentV}.h${this.currentH}`).addClass('motherShip'))
+    console.log(this.currentH, this.currentV)
+    $(`.v${this.currentV}.h${this.currentH}`).addClass('alienShip1')
   }
 
-  movement() {
-    this.movementTimer = setInterval(() => {
-      $(`.v${this.currentV}.h${this.currentH}`).removeClass('motherShip')
-      this.currentV++
+  move() {
+    this.movementId = setInterval(() => {
+      // invadersInterval = setInterval(invaders())
+      $(`.v${this.currentV}.h${this.currentH}`).removeClass('alienShip1')
+      if (this.currentMoves < 5) {
+        console.log(this.currentMoves)
+        this.currentMoves++
+        if(this.isMovingRight) {
+          this.currentV++
+        } else {
+          this.currentV--
+        }
+      } else {
+        this.currentH++
+        this.currentMoves = 0
+        this.isMovingRight = !this.isMovingRight
+      }
       this.render()
-      console.log(aliens)
     }, alienShipSpeed)
   }
 }
 
-
-
-function placeAlienShips () {
-
-
-  // * Move a single ship
-  // * left one
-  // * left one
-  // * right one
-  // * right one
-  // * right one
-  // * right one
-  // * left one
-  // * left one
-}
+// class Alien {
+//   constructor(startingV, startingH, name, score) {
+//     this.currentV = startingV
+//     this.currentH = startingH
+//     this.name = name
+//     this.score = score
+//     this.isHit = false
+//     this.newCurrentV = null
+//     this.newCurrentH = null
+//     this.render()
+//     this.movementTimer = null
+//     this.movement()
+//   }
+//
+//   render() {
+//     $(`.v${this.currentV}.h${this.currentH}`).addClass('alienShip1')
+//   }
+//
+//   movement() {
+//     this.newCurrentV = this.currentV++
+//     console.log(`v${this.newCurrentV}`)
+//     console.log(`v${this.CurrentV}`)
+//     $(`.v${this.newCurrentV}.h${this.currentH}`).addClass('alienShip1')
+//     $(`.v${this.currentV}.h${this.currentH}`).removeClass('alienShip1')
+//     this.currentV = this.newCurrentV
+//     console.log(`v${this.CurrentV}`)
+//     this.render()
+//     console.log(aliens)
+//   }
+// }
+//   movement() {
+//     this.movementTimer = setInterval(() => {
+//       $(`.v${this.currentV}.h${this.currentH}`).removeClass('alienShip1')
+//       this.currentV++
+//       this.render()
+//       console.log(aliens)
+//     }, alienShipSpeed)
+//   }
+// }
 
 function motherShipMover () {
-  if (motherShipLocation === 'v13') {
+  if (motherShipLocation === 'v25') {
     clearInterval(motherShipMoverInterval)
     motherShipIntervalFrequency()
   } else {
@@ -155,8 +241,8 @@ function playerShipLocator () {
 function playerShipMovement (newLocation, oldLocation) {
   switch (newLocation === oldLocation) {
     case false:
-      $(`div.h11.${newLocation}`).addClass('playerShip')
-      $(`div.h11.${oldLocation}`).removeClass('playerShip')
+      $(`div.h22.${newLocation}`).addClass('playerShip')
+      $(`div.h22.${oldLocation}`).removeClass('playerShip')
       playerShipLocation = newLocation
       break
     default:
@@ -173,8 +259,8 @@ function ShipMovementValue (movement) {
   const numberOnly = parseInt(playerShipLocation.replace('v', ''))
   console.log(movement)
   const newLocation = numberOnly + movement
-  if (newLocation > 12) {
-    return 'v12'
+  if (newLocation > 24) {
+    return 'v24'
   } else if  (newLocation < 1) {
     return 'v1'
   } else {
